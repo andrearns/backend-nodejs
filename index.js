5d0ff8e2715e19e7c2c82fbdd8d978573d103706
André Arns <andreluisarns@gmail.com>
2021-03-08 12:22:49 -0300

    Atualização das requisições HTTP

--- index.js
+++ index.js
@@ -1,54 +1,74 @@
 const express = require('express'); // Criando uma variável importando o express do seu arquivo
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
+const projects = [];
+
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+}
+
 /** `GET` →  Quando queremos buscar uma informação no back-end**/
 app.get('/projects', (request, response) => {
     const { title, owner } = request.query; // Usando query params!!! 
 
-    console.log(title);
-    console.log(owner);
+    const results = title ? projects.filter(project => project.title.includes(title)) : projects;
 
-    return response.json([ // Cria uma resposta em arquivo JSON para ser enviada quando um usuário acessar a rota localhost:3333
-        'Projeto 1',
-        'Projeto 2',
-        ]);
+    return response.json(results);
 });  
 
 /** `POST` → Quando queremos criar uma informação no back-end*/   
-app.post('/projects', (request, response) =>{
-    const body = request.body; // Usando Request Body!!!
+app.post('/projects', (request, response) => {
+    const { title, owner } = request.body;
 
-    console.log(body);
+    const project = { id: uuid(), title, owner};
+    
+    projects.push(project);
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    return response.json(project);
 });
 
 /** `PUT/PATCH` →  Quando queremos alterar uma informação no no back-end*/  
 app.put('/projects/:id', (request, response) =>{
-    const params = request.params; // Usando route params para especificar os elementos!!!
+    const { id } = request.params; // Usando route params para especificar os elementos!!!
 
-    console.log(params);
+    const projectIndex = projects.findIndex(project => project.id == id);
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found." })
+    }
+
+    const project = {
+        id, 
+        title,
+        owner
+    }
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
 });
 
 /** `DELETE` →  Quando queremos deletar uma informação no no back-end */   
 app.delete('/projects/:id', (request, response) =>{
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found." })
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.send();
 });
 
 app.listen(3333, () => { // Colocamos uma função que é ativada instantaneamente quando o servidor está no ar
